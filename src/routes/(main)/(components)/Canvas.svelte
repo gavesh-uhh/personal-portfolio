eaa4dca8169e52a99d0bb8a477ff9f7fa4700160
Gavesh Saparamadu <gaveshasaparamadu06@gmail.com>
2024-10-17 19:26:07 +0530

    [Feature] Added Canvas
    Blurred Circle that follows the mouse for UI

---
+++ src/routes/(main)/(components)/Canvas.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let boxObject: HTMLElement;
+	let mouseX = 0;
+	let mouseY = 0;
+	let currentX = 0;
+	let currentY = 0;
+
+	function handleMouseMove(event: MouseEvent) {
+		mouseX = event.clientX;
+		mouseY = event.clientY;
+	}
+
+	function handleTouchMove(event: TouchEvent) {
+		mouseX = event.touches[0].clientX;
+		mouseY = event.touches[0].clientY;
+	}
+
+	function lerp(start: number, end: number, t: number) {
+		return start + (end - start) * t;
+	}
+
+	function animate() {
+		currentX = lerp(currentX, mouseX - 50, 0.1);
+		currentY = lerp(currentY, mouseY - 50, 0.1);
+		if (!boxObject.style == null) return;
+		boxObject.style.left = `${currentX}px`;
+		boxObject.style.top = `${currentY}px`;
+		requestAnimationFrame(animate);
+	}
+
+	onMount(() => {
+		window.addEventListener('mousemove', handleMouseMove);
+		window.addEventListener('touchmove', handleTouchMove);
+		animate();
+
+		return () => {
+			window.removeEventListener('mousemove', handleMouseMove);
+			window.removeEventListener('touchmove', handleTouchMove);
+		};
+	});
+</script>
+
+<div class="motion-reduce:hidden absolute w-full left-0 top-0 h-screen">
+	<div id="object" bind:this={boxObject}></div>
+</div>
+
+<style>
+	#object {
+		transition: all;
+		position: absolute;
+		width: 100px;
+		height: 100px;
+		left: -30px;
+		top: -30px;
+		opacity: 50%;
+		border-radius: 50%;
+		filter: blur(45px);
+		background-color: seagreen;
+	}
+</style>
+
--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import '../../app.css';
+	import Canvas from './(components)/Canvas.svelte';
 	import Header from './(components)/Header.svelte';
 	import Navigation from './(components)/Navigation.svelte';
 	import { fade } from 'svelte/transition';
@@ -14,16 +15,19 @@
 </svelte:head>
 
 <div class="py-12 px-8 sm:p-16 min-h-screen flex flex-col justify-between">
-	<Header />
-	<Navigation />
-	<hr class="mb-4 mt-0 opacity-15" />
-	{#key pathname}
-		<div
-			class="mt-2 sm:mt-0 flex-grow"
-			in:fade={{ duration: 300, delay: 100 }}
-			out:fade={{ duration: 100, delay: 50 }}
-		>
-			<slot />
-		</div>
-	{/key}
+	<div class="z-10">
+		<Header />
+		<Navigation />
+		<hr class="mb-4 mt-0 opacity-15" />
+		{#key pathname}
+			<div
+				class="mt-2 sm:mt-0 flex-grow"
+				in:fade={{ duration: 300, delay: 100 }}
+				out:fade={{ duration: 100, delay: 50 }}
+			>
+				<slot />
+			</div>
+		{/key}
+	</div>
+	<Canvas />
 </div>
